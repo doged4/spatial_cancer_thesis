2547338a14a0f273bd463516c7c213f2632e18ae
Cevi Bainton <baintoncmv@gmail.com>
2023-10-10 18:34:14 -0700

    first full resolution image import testing

---
+++ image_intake_debugging.py
@@ -0,0 +1,27 @@
+# %%
+import os
+os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = pow(2,40).__str__()
+import cv2
+
+
+# %%
+# print(os.environ["CV_IO_MAX_IMAGE_PIXELS"])
+
+# %% [markdown]
+# This script is intended as a basic test of methods to split and input images for the model creation.
+
+# %%
+high_res_path = "original_data\High-resolution_tissue_images"
+patient_tester = "33"
+patient_tester_first = R'C:\Users\cbainton\Desktop\ST_project\original_data\High-resolution_tissue_images\V10F03-033\201210_BC_V10F03-033_S8C-T_RJ.A1-Spot000001.jpg'
+
+# Fails to import as too large 
+cv2.imread(patient_tester_first)
+
+# Imports and shows fine 
+# first_lbshot = cv2.imread(R'C:\Users\cbainton\Desktop\ST_project\original_data\LB-screenshots\33A.png')
+# cv2.imshow("First LB screen shot", first_lbshot)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
