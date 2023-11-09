f0b913724fd94fb153f02d9fad4c75a40509b32c
Cevi Bainton <baintoncmv@gmail.com>
2023-11-08 16:49:17 -0800

    Added intermediate tf Dataset step per sample

--- testing_tf_models.py
+++ testing_tf_models.py
@@ -2,7 +2,7 @@
 import os
 import itertools
 
-import matplotlib.pylab as plt
+# import matplotlib.pylab as plt
 import numpy as np
 
 import tensorflow as tf
@@ -18,7 +18,12 @@ import pickle
 
 print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
 
+# sess = tf.Session()
+# run_metadata = tf.RunMetadata()
+# sess.run(c, options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True), run_metadata=run_metadata)
 
+# with open("im_features/run2.txt", "w") as out:
+#   out.write(str(run_metadata))
 
 # ### Effnet_b4 below
 # # %% Load in efficientnet_b4, which is the right size: EASY TO CHANGW
@@ -56,50 +61,89 @@ print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT 
 ### Effnet v2l as paper recommended
 # %% Load in data
 IMAGE_SIZE = (480, 480, 3)
+
+# Load in data from jpg files to make TF Dataset objects
 PATCHED_PATH = R"C:\Users\cbainton\Desktop\ST_project\patched_data_480"
 OUT_DIR = "im_features"
+OUT_DATASET = "tf_dataset\processed_st"
 
-# %% Set up model
-effnet = tf.keras.applications.efficientnet_v2.EfficientNetV2L(
-    include_top=True,
-    weights='imagenet',
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
-    classes=1000,
-    classifier_activation='softmax',
-    include_preprocessing=True
-)
+
+
 # %% Sample setup
-sample = "V10F03-033_C"
-sample_dir = os.path.join(PATCHED_PATH, sample, "patches")
-image_paths = os.listdir(sample_dir)
-sample_out = os.path.join(OUT_DIR, sample)
+# sample = "V10F03-033_C"
+samples = os.listdir(PATCHED_PATH)
 
-if not os.path.exists(sample_out):
-    os.mkdir(sample_out)
+def save_dataset(sample):
+    sample_dir = os.path.join(PATCHED_PATH, sample, "patches")
+    image_paths = os.listdir(sample_dir)
+    data_out = os.path.join(OUT_DATASET, sample)
+    print(f"Sample: {sample}")
 
-# %% Read in images
-images = np.ndarray((len(image_paths),) + IMAGE_SIZE)
-for i in range(len(image_paths)):
-    image_path = image_paths[i]
-    image_arr = cv2.imread(os.path.join(sample_dir, image_path))
-    images[i] = image_arr
+    if not os.path.exists(data_out):
+        os.mkdir(data_out)
 
-# plt.imshow(images[0].astype(int))
-# plt.show()
+    # Read in images
+    images = np.ndarray((len(image_paths),) + IMAGE_SIZE)
+    for i in range(len(image_paths)):
+        image_path = image_paths[i]
+        image_arr = cv2.imread(os.path.join(sample_dir, image_path))
+        images[i] = image_arr
 
-# %% Run predictions
-image_outputs = effnet.predict(images)
+    dataset_names = tf.constant([name[:-4] for name in image_paths])
+    dataset = tf.data.Dataset.from_tensor_slices((images, dataset_names))
+    dataset.save(data_out)
+    print(f"Successfully output to {data_out}")
 
+
+def get_features(sample):
+    print(sample)
+    print(f'peak {tf.config.experimental.get_memory_info("GPU:0")["peak"]/1000000} mb')
+    print(f'current {tf.config.experimental.get_memory_info("GPU:0")["current"]/1000000} mb')
+
+    # sample_dir = os.path.join(PATCHED_PATH, sample, "patches")
+    image_paths = os.listdir(sample_dir)
+    sample_out = os.path.join(OUT_DIR, sample)
+
+    if not os.path.exists(sample_out):
+        os.mkdir(sample_out)
+
+    # Read in images
+    images = np.ndarray((len(image_paths),) + IMAGE_SIZE)
+    for i in range(len(image_paths)):
+        image_path = image_paths[i]
+        image_arr = cv2.imread(os.path.join(sample_dir, image_path))
+        images[i] = image_arr
+
+    test =tf.data.Dataset.from_tensor_slices(images)
+    # plt.imshow(images[0].astype(int))
+    # plt.show()
+
+    # Run predictions
+    image_outputs = effnet.predict(images)
+
+    # 
+    # Improve this output method in future
+    #   improve spot naming
+    #   improve format?
+    image_out_dict = {}
+    for i in range(len(image_paths)):
+        image_out_dict[image_paths[i]] = image_outputs[i]
+
+    out_dict =  os.path.join(sample_out, "image_features_dict.pickle")
+    pickle.dump(image_out_dict, open(out_dict, "wb"))
+
+# # %% Set up model
+# effnet = tf.keras.applications.efficientnet_v2.EfficientNetV2L(
+#     include_top=True,
+#     weights='imagenet',
+#     input_tensor=None,
+#     input_shape=None,
+#     pooling=None,
+#     classes=1000,
+#     classifier_activation='softmax',
+#     include_preprocessing=True
+# )
 # %%
-# Improve this output method in future
-#   improve spot naming
-#   improve format?
-image_out_dict = {}
-for i in range(len(image_paths)):
-    image_out_dict[image_paths[i]] = image_outputs[i]
-
-out_dict =  os.path.join(sample_out, "image_features_dict.pickle")
-pickle.dump(image_out_dict, open(out_dict, "wb"))
+for t_sample in samples:
+    save_dataset(t_sample)
 # %%
