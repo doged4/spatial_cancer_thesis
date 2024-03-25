47b70dc6b775082d2dc41c727fede9eebb278ff6
Cevi Bainton <baintoncmv@gmail.com>
2024-03-24 23:43:50 -0700

    analyzed preliminary cv mt elasticnet model

--- prediction_testing.py
+++ prediction_testing.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt # In case simple plots needed
 
 from anndata import read_h5ad
 import pandas as pd
+import scanpy as sc
 
 import pickle
 import logging
@@ -122,9 +123,57 @@ def model2(im_features_ad, enrichments_ad):
 
 # %% Retrieve model
 
-# mt_elnet = pickle.load(open('intermediate_data/saved_elnet.pickle','rb'))
+mt_elnet = pickle.load(open('intermediate_data/saved_elnet.pickle','rb'))
+# %% [markdown]
+# ### Looking at our model
+mt_elnet = pickle.load(open('intermediate_data/saved_cv_elnet.pickle','rb'))
+# Selected parameters:
+mt_elnet.alpha_
+mt_elnet.l1_ratio_
 
+mt_elnet.intercept_
 
+# %% [markdown]
+# Let's look at our coefficients
+mt_elnet.coef_
+# Seems like a lot of zeroes... how many features aren't included?
+# Every feature that is all zero:
+sum([all([x == 0 for x in y]) for y in mt_elnet.coef_.transpose()])
+# What is not all zeroes?
+used_features = [not all([x == 0 for x in y]) for y in mt_elnet.coef_.transpose()]
+used_features_names = mt_elnet.feature_names_in_[used_features]
+print(used_features_names)
+
+# %% [markdown]
+# # Evaluating our model
+im_features_ad = read_h5ad("intermediate_data/with_image_features_33D_S8T2_2.h5ad")
+enrichments_ad = read_h5ad("intermediate_data/s8t2_all_at_once_enrichments.h5ad")
+
+# This doesn't seem good
+mt_elnet.score(
+    X = im_features_ad.to_df(),
+    y = enrichments_ad.to_df()
+)
+# %% [markdown]
+# That's not good! That's quite concerning. What do these features look like?
+sc.pl.spatial(im_features_ad, color = used_features_names)
+# %% [markdown]
+# It looks like these image features are pretty sparse. This could mean a couple of things.
+# I trust the regularization, but let's go back to figure out what the best model would capture.
+linear_output = linear_model.LinearRegression()
+linear_output.fit(
+    X = im_features_ad.to_df(),
+    y = enrichments_ad.to_df())
+# Here's what our best could be:
+print(linear_output.score(
+    X = im_features_ad.to_df(),
+    y = enrichments_ad.to_df()))
+# %% [markdown]
+# Seems like a lot of our features are being eliminated. This could either mean that our regulization is too agressive, or that we just do not have any significantly effective features.
+# This leaves us with two options:
+# 
+# * Refit the model with different parameters
+# * Transfer learning train our model to do enrichment predictions
 # %% Backround logging info
 
 
